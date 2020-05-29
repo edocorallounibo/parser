275d9d07fe4565c77bb697461c9e04e46bee2815
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-05-29 20:12:36 +0200

    Update drain3_parser.py

--- drain3_parser.py
+++ drain3_parser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 
 """
 Description : Example of using Drain3 with Kafka persistence
 Author      : David Ohana, Moshik Hershcovitch, Eran Raichstein
@@ -21,34 +23,45 @@ from drain3.kafka_persistence import KafkaPersistence
 
 parser=argparse.ArgumentParser()
 group=parser.add_mutually_exclusive_group()
-parser.add_argument("logfile",type=str,help="Name of the file you want to parse.")
+parser.add_argument("log_file",type=str,help="Name of the file you want to parse.")
 group.add_argument("-f","--frontend",action="store_true",help="Used if you want to parse a storm-frontend log file.")
 group.add_argument("-b","--backend",action="store_true",help="Used if you want to parse a storm-backend log file.")
 args=parser.parse_args()
-logfile=args.logfile
+log_file=args.log_file
 if args.frontend:
         log_type="storm-frontend"
         input_dir = 'frontend-server/'
-        print("Parsing {} as a frontend log file..".format(logfile))
+        print("Parsing {} as a frontend log file..".format(log_file))
         log_format = '<Date> <Time> <Pid> - <Level> <Component>: <Content>'#Frontend logformat
 elif args.backend:
         log_type="storm-backend"
         input_dir = 'backend-server/'
-        print("Parsing {} as a backend log file..".format(logfile))
+        print("Parsing {} as a backend log file..".format(log_file))
         log_format = '<Time> - <Level> <Component> - <Content>'#Backend logformat
-
-
+try:
+    os.mkdir("results")
+except OSError:
+    pass
+else:
+    print("results directory created")
+try:
+    os.mkdir("results/{}".format(log_type))
+except OSError:
+    pass
+else:
+    print("{} directory made".format(log_type))
+    
 ################
 def load_data():
         headers, regex =generate_logformat_regex(log_format)
-        return log_to_dataframe(os.path.join(input_dir, logfile), regex, headers, log_format)
+        return log_to_dataframe(os.path.join(input_dir, log_file), regex, headers, log_format)
 
-def log_to_dataframe(log_file, regex, headers, logformat):
+def log_to_dataframe(logfile, regex, headers, logformat):
         """ Function to transform log file to dataframe 
         """
         log_messages = []
         linecount = 0
-        with open(log_file, 'r') as fin:
+        with open(logfile, 'r') as fin:
             for line in fin.readlines():
                 line = re.sub(r'[^\x00-\x7F]+', '<NASCII>', line)
                 try:
@@ -61,6 +74,12 @@ def log_to_dataframe(log_file, regex, headers, logformat):
         logdf = pd.DataFrame(log_messages, columns=headers)
         logdf.insert(0, 'LineId', None)
         logdf['LineId'] = [i + 1 for i in range(linecount)]
+        if os.path.isfile("results/{}/{}_struct.csv".format(log_type,log_file)):
+            pass
+        else:
+            file_out=open("results/{}/{}_struct.csv".format(log_type,log_file),"w")
+            logdf.to_csv(file_out)
+            file_out.close()
         return logdf
 def generate_logformat_regex(logformat):
         """ Function to generate regular expression to split log messages
@@ -95,7 +114,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
 if persistence_type == "KAFKA":
     persistence = KafkaPersistence("localhost:9092", "drain3_state")
 elif persistence_type == "FILE":
-    persistence = FilePersistence("drain3_state[{}].bin".format(log_type))
+    persistence = FilePersistence("results/{}/drain3_state[{}].bin".format(log_type,log_type))
 else:
     persistence = None
 
@@ -108,6 +127,8 @@ for idx, line in load_data().iterrows():
         #component=line['Component']
         #level=line['Level']
         result=template_miner.add_log_message(line['Content'])
+        result_json = json.dumps(result)
+        print(result_json)
         #result_json=json.dump(result)
         
 print("Clusters:")

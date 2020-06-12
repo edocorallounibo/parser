fb45e2ab2a90d702f0bbe7b8b40c2b0d1fe62e33
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-06-12 21:17:42 +0200

    Update drain_sequences.py

--- drain_sequences.py
+++ drain_sequences.py
@@ -9,16 +9,14 @@ import configparser
 import json
 import logging
 import sys
-sys.path.append('../')
 import argparse
 import pandas as pd
 import re
-import importlib
-importlib.import_module('drain3')
-
+from datetime import datetime as dt
 from drain3 import TemplateMiner
 from drain3.file_persistence import FilePersistence
 from drain3.kafka_persistence import KafkaPersistence
+
 
 parser=argparse.ArgumentParser(prog='drain_sequences')
 group=parser.add_mutually_exclusive_group()
@@ -28,12 +26,11 @@ group.add_argument("-b","--backend",action="store_true",help="Used if you want t
 args=parser.parse_args()
 log_file=args.log_file
 if args.frontend:
-        log_type="storm-frontend"
-        #input_dir = 'frontend-server/'
+        log_type="frontend-server"
+elif args.backend:
+        log_type="backend-server"
 else:
-        log_type="storm-backend"
-        #input_dir = 'backend-server/'
-
+        print("this line is never printed and thus useless!")
 
 # persistence_type = "NONE"
 # persistence_type = "KAFKA"
@@ -56,20 +53,39 @@ df=pd.read_csv("results/{}/{}_struct.csv".format(log_type,log_file))
 comp=df.loc[:,"Component"]
 content=df.loc[:,"Content"]
 level=df.loc[:,"Level"]
+time=df.loc[:,"Time"]
 comp_clustid={}
+comp_time={}
 comp_clustid_abnormal={}
+comp_time_abnormal={}#WIP:only based on "ERROR" level
 for i in content.index:
     result = template_miner.add_log_message(content[i])
     clustid=re.sub(r'A0*','',result["cluster_id"])
     if comp[i] not in comp_clustid.keys():
         comp_clustid[str(comp[i])]=[str(clustid)]
+       
     else:
         comp_clustid[str(comp[i])].append(str(clustid))
+        
+for i in time.index:
+        if comp[i] not in comp_time.keys():
+            comp_time[str(comp[i])]=[str(time[i])]
+        else:
+            comp_time[str(comp[i])].append(str(time[i]))
+for x in comp_time.keys():
+    for i in range(len(comp_time[x])):
+        i+=1
+        if i<len(comp_time[x]):
+            comp_time[x][-i]=str((dt.strptime(comp_time[x][-i],'%H:%M:%S.%f')-dt.strptime(comp_time[x][-(i+1)],'%H:%M:%S.%f')).total_seconds())
+        else:
+            comp_time[x][0]='0'
 for i in comp.index:
     if level[i]=="ERROR":
         if comp[i] in comp_clustid:
             comp_clustid_abnormal[str(comp[i])]=comp_clustid[str(comp[i])]
+            comp_time_abnormal[str(comp[i])]=comp_time[str(comp[i])]
             del comp_clustid[str(comp[i])]
+            del comp_time[str(comp[i])]
 
 abnormal=open("results/{}/{}_abnormal".format(log_type,log_file),"w")
 for x in comp_clustid_abnormal.keys():
@@ -87,4 +103,18 @@ for x in comp_clustid.keys():
               normal.write(" ")
          normal.write("\n")
 normal.close()
-    
+abnormal=open("results/{}/{}_timestamps_abnormal".format(log_type,log_file),"w")
+for x in comp_time.keys():
+         for i in range(len(comp_time[x])): 
+              abnormal.write(str(comp_time[x][i]))
+              abnormal.write(" ")
+         abnormal.write("\n")
+abnormal.close()
+normal=open("results/{}/{}_timestamps_normal".format(log_type,log_file),"w")
+for x in comp_time.keys():
+    if (len(comp_time[x])>=10):
+         for i in range(len(comp_time[x])): 
+              normal.write(str(comp_time[x][i]))
+              normal.write(" ")
+         normal.write("\n")
+normal.close()   

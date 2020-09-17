6fda0656b8ff1ea1566f5c1a6061fa9f0937454c
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-09-17 12:30:30 +0200

    Update log_to_dataframe.py

--- log_to_dataframe.py
+++ log_to_dataframe.py
@@ -13,13 +13,15 @@ log_file=args.log_file
 if args.frontend:
         log_type="frontend-server"
         input_dir = '/container/logfiles/frontend-server/'
-        print("Parsing {} with {} format".format(log_file,log_format))
         log_format = '<Date> <Time> <Pid> - <Level> <Component>: <Content>'#Frontend logformat
+        print("Parsing {} with [{}] format...".format(log_file,log_format))
+        
 elif args.backend:
         log_type="backend-server"
         input_dir = '/container/logfiles/backend-server/'
-        print("Parsing {} as a backend log file..".format(log_file))
         log_format = '<Time> - <Level> <Component> - <Content>'#Backend logformat
+        print("Parsing {} with [{}] format...".format(log_file,log_format))
+        
         
 def load_data():
         headers, regex = generate_logformat_regex(log_format)

11cf5606177b1c3e45230692c244163400dc31bb
edocorallo <58848670+edocorallo@users.noreply.github.com>
2020-11-10 11:37:21 +0100

    Create nseq.py

---
+++ nseq.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 
+"""
+Created on Fri May 22 17:36:45 2020
+
+@author: edoardo
+"""
+import configparser
+import json
+import logging
+import sys
+import argparse
+import pandas as pd
+import re
+from datetime import datetime as dt
+from drain3 import TemplateMiner
+from drain3.file_persistence import FilePersistence
+from drain3.kafka_persistence import KafkaPersistence
+
+
+parser=argparse.ArgumentParser(prog='drain_sequences')
+group=parser.add_mutually_exclusive_group()
+parser.add_argument("log_file",type=str,help="Name of the file you want to parse.")
+group.add_argument("-f","--frontend",action="store_true",help="Used if you want to parse a storm-frontend log file.")
+group.add_argument("-b","--backend",action="store_true",help="Used if you want to parse a storm-backend log file.")
+args=parser.parse_args()
+log_file=args.log_file
+if args.frontend:
+        log_type="frontend-server"
+elif args.backend:
+        log_type="backend-server"
+else:
+        print("this line is never printed and thus useless!")
+
+# persistence_type = "NONE"
+# persistence_type = "KAFKA"
+persistence_type = "FILE"
+
+config = configparser.ConfigParser()
+config.read('drain3.ini')
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
+if persistence_type == "KAFKA":
+    persistence = KafkaPersistence("localhost:9092", "drain3_state")
+elif persistence_type == "FILE":
+    persistence = FilePersistence("results/{}/drain3_state[{}].bin".format(log_type,log_type))
+else:
+    persistence = None
+template_miner = TemplateMiner(persistence)
+df=pd.read_csv("results/{}/{}_struct.csv".format(log_type,log_file))
+comp=df.loc[:,"Component"]
+content=df.loc[:,"Content"]
+level=df.loc[:,"Level"]
+time=df.loc[:,"Time"]
+comp_clustid={}
+comp_time={}
+comp_clustid_abnormal={}
+comp_time_abnormal={}
+fail_pattern=re.compile(r'FAILURE')
+error_pattern=re.compile(r'ERROR')
+
+for i in content.index:
+    result = template_miner.add_log_message(content[i])
+    clustid=re.sub(r'A0*','',result["cluster_id"])
+    if comp[i] not in comp_clustid.keys():
+        comp_clustid[str(comp[i])]=[str(clustid)]
+       
+    else:
+        comp_clustid[str(comp[i])].append(str(clustid))
+        
+for i in time.index:
+        if comp[i] not in comp_time.keys():
+            comp_time[str(comp[i])]=[str(time[i])]
+        else:
+            comp_time[str(comp[i])].append(str(time[i]))
+for x in comp_time.keys():
+    for i in range(len(comp_time[x])):
+        i+=1
+        if i<len(comp_time[x]):
+            comp_time[x][-i]=str((dt.strptime(comp_time[x][-i],'%H:%M:%S.%f')-dt.strptime(comp_time[x][-(i+1)],'%H:%M:%S.%f')).total_seconds())
+        else:
+            comp_time[x][0]='0'
+for i in comp.index:
+    match_error=re.search(error_pattern,content[i])
+    match_failure=re.search(fail_pattern,content[i])
+    if match_error or match_failure:
+        if comp[i] in comp_clustid:
+            comp_clustid_abnormal[str(comp[i])]=comp_clustid[str(comp[i])]
+            comp_time_abnormal[str(comp[i])]=comp_time[str(comp[i])]
+            del comp_clustid[str(comp[i])]
+            del comp_time[str(comp[i])]
+
+
+normal=open("results/{}/{}_normal".format(log_type,log_file),"w")
+for x in comp_clustid.keys():
+    #if (len(comp_clustid[x])>=10):
+         for i in range(len(comp_clustid[x])): 
+              normal.write(str(comp_clustid[x][i]))
+              normal.write(" ")
+         normal.write("\n")
+normal.close()
